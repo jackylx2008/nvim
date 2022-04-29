98e1cf5d704c6c778ec090824555af249f852e0e
jackylx2008 <jackylx2008@hotmail.com>
2022-04-29 17:01:57 +0800

    2022-04-29 17:00 WSL

--- init.lua
+++ init.lua
@@ -22,9 +22,13 @@ require("user.alpha")
 require("user.hop_conf")
 
 -- Disable visual text from LSP 就是禁用每行后面语法检查的提示，要不敲代码的时候后面一直有这个提示，比较烦人
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	virtual_text = false,
-})
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+    vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics,
+    {
+        virtual_text = false
+    }
+)
 
 vim.g.ZFVimIM_openapi_enable = 1
 vim.g.ZFVimIM_pinyin_gitUserEmail = "jackylx2008@hotmail.com"
