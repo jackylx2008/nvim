fa5584c5afd6e2120df866ed76aad68bbb3dc32c
jackylx2008 <jackylx2008@hotmail.com>
2022-04-29 09:12:20 +0800

    2022-04-29 wsl worked

--- lua/user/nvim_tree.lua
+++ lua/user/nvim_tree.lua
@@ -1,122 +1,59 @@
-local g = vim.g
-g.nvim_tree_highlight_opened_files = 1
--- g.nvim_tree_indent_markers = 1
--- 目录后加上反斜杠 /
-g.nvim_tree_add_trailing = 1
+-- https://github.com/kyazdani42/nvim-tree.lua
 
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
+local icons = require("utils.icons")
+
+-- add a backslash / after the directory
+vim.g.nvim_tree_add_trailing = 1
+
 vim.g.nvim_tree_icons = {
-	default = "",
-	symlink = "",
-	git = {
-		unstaged = "",
-		staged = "✓",
-		unmerged = "",
-		renamed = "➜",
-		deleted = "",
-		untracked = "U",
-		ignored = "◌",
-	},
-    renderer = {
-        indent_markers = {
-          enable = true,
-          icons = {
-            corner = "└ ",
-            edge = "│ ",
-            none = "  ",
-          },
-        },
+    default = " ",
+    symlink = " ",
+    git = {
+        unstaged = "",
+        staged = "",
+        unmerged = "",
+        renamed = "凜",
+        untracked = "",
+        deleted = "",
+        ignored = ""
     },
-	folder = {
-		default = "",
-		open = "",
-		empty = "",
-		empty_open = "",
-		symlink = "",
-		-- arrow_open = "╰─▸",
-		-- arrow_closed = "├─▸",
-		arrow_open = "",
-		arrow_closed = "",
-		symlink_open = "",
-	},
+    folder = {
+        -- arrow_open = "╰─▸",
+        -- arrow_closed = "├─▸",
+        arrow_open = "",
+        arrow_closed = "",
+        default = "",
+        open = "",
+        empty = "",
+        empty_open = "",
+        symlink = "",
+        symlink_open = ""
+    }
 }
 
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
-if not status_ok then
-	return
-end
+require("nvim-tree").setup(
+    {
+        diagnostics = {
+            enable = true,
+            show_on_dirs = true,
+            icons = {
+                hint = icons.diagnostics.hint,
+                info = icons.diagnostics.info,
+                warning = icons.diagnostics.warning,
+                error = icons.diagnostics.error
+            }
+        }
+    }
+)
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-	return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-nvim_tree.setup({
-	disable_netrw = true,
-	hijack_netrw = true,
-	open_on_setup = false,
-	ignore_ft_on_setup = {
-		"startify",
-		"dashboard",
-		"alpha",
-	},
-	-- auto_close = true,
-	open_on_tab = false,
-	hijack_cursor = false,
-	update_cwd = true,
-	update_to_buf_dir = {
-		enable = true,
-		auto_open = true,
-	},
-	diagnostics = {
-		enable = true,
-		icons = {
-			hint = "",
-			info = "",
-			warning = "",
-			error = "",
-		},
-	},
-	update_focused_file = {
-		enable = true,
-		update_cwd = true,
-		ignore_list = {},
-	},
-	git = {
-		enable = true,
-		ignore = true,
-		timeout = 500,
-	},
-	view = {
-		width = 30,
-		height = 30,
-		hide_root_folder = false,
-		side = "left",
-		auto_resize = true,
-		mappings = {
-			custom_only = false,
-			list = {
-				{ key = { "<CR>" }, cb = tree_cb("edit") },
-				{ key = "h", cb = tree_cb("close_node") },
-				{ key = "v", cb = tree_cb("vsplit") },
-			},
-		},
-		number = false,
-		relativenumber = false,
-	},
-	quit_on_open = 0,
-	git_hl = 1,
-	disable_window_picker = 0,
-	root_folder_modifier = ":t",
-	show_icons = {
-		git = 1,
-		folders = 1,
-		files = 1,
-		folder_arrows = 1,
-		tree_width = 30,
-	},
-})
-vim.cmd([[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]])
+vim.api.nvim_create_autocmd(
+    {"BufEnter"},
+    {
+        pattern = {"*"},
+        callback = function()
+            if vim.fn.winnr("$") == 1 and vim.fn.bufname() == "NvimTree_" .. vim.fn.tabpagenr() then
+                vim.cmd("quit")
+            end
+        end
+    }
+)
