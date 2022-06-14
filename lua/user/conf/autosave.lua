1714a3a43f90d15170ea5bb9d4999d0ba4515aa2
jackylx2008 <jackylx2008@hotmail.com>
2022-06-14 10:02:52 +0800

    2022-06-14 .gitignore

--- lua/user/conf/autosave.lua
+++ lua/user/conf/autosave.lua
@@ -1,23 +1,25 @@
 -- https://github.com/Pocco81/AutoSave.nvim
 local status_ok, autosave = pcall(require, "autosave")
 if not status_ok then
-	vim.notify("autosave not found!")
-	return
+    vim.notify("autosave not found!")
+    return
 end
 
-autosave.setup({
-	enabled = true,
-	-- execution_message = "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"),
-	execution_message = "",
-	events = { "InsertLeave", "TextChanged" },
-	conditions = {
-		exists = true,
-		filename_is_not = { "plugins.lua" },
-		filetype_is_not = { "lua" },
-		modifiable = true,
-	},
-	write_all_buffers = false,
-	on_off_commands = true,
-	clean_command_line_interval = 0,
-	debounce_delay = 135,
-})
+autosave.setup(
+    {
+        enabled = false,
+        -- execution_message = "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"),
+        execution_message = "",
+        events = {"InsertLeave", "TextChanged"},
+        conditions = {
+            exists = true,
+            filename_is_not = {"plugins.lua"},
+            filetype_is_not = {"lua"},
+            modifiable = true
+        },
+        write_all_buffers = false,
+        on_off_commands = true,
+        clean_command_line_interval = 0,
+        debounce_delay = 135
+    }
+)
